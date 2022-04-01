a95efabfdedc5e5570a33c94ee9e21f9dc112b32
Akila_Ayanthi <aayanthi@gmail.com>
2022-04-01 09:30:02 +0530

    dae images

--- multiview_adv_images.py
+++ multiview_adv_images.py
@@ -163,11 +163,11 @@ def gen_images(width, height, savename, gt, file_name):
         imgname = '/'.join(imgfile)
         sname = savename + imgname
         # imgname = '/'.join(sname)
-        print(sname)
+        print(sname.split('/')[:7])
 
 
-        # if not os.path.exists(sname.split('/')[:9]):
-        #     os.makedirs(sname.split('/')[:9])
+        # if not os.path.exists(sname.split('/')[:7]):
+        #     os.makedirs(sname.split('/')[:7])
         
         # img, bbox = plot_boxes_cv2(img, boxes[0], sname, class_names)
 
